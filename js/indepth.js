ea1f3f7e77b88833a386774f827f85e4250d7883
Vocho <vocho@underdog.gs>
2016-04-26 10:14:18 -0500

    ijnj
    njnj

--- js/indepth.js
+++ js/indepth.js
@@ -876,8 +876,8 @@ $("#indepth_twittear").click(function(){
 
 		
 		var text = encodeURIComponent("Mi predicción es: Atletico de Madrid "+$("input[name=goleador]").val()+"-"+$("input[name=goleador2]").val())+ " Bayern Munich primer gol al minuto "+$("input[name=goles_anotados]").val()+" @juanfutbol";
-		var url = encodeURIComponent("http://juanfutbol.com/indepth/");
-		window.open("https://twitter.com/share?text="+text+"&hashtags=juanfutbol&url="+url,"","width=500, height=300");
+		var url = encodeURIComponent("http://juanfutbol.com/indepth/juanpizza-champions-2");
+		window.open("https://twitter.com/share?text="+text+"&hashtags=PizzaFut&url="+url,"","width=500, height=300");
 
 	}else{
 		
